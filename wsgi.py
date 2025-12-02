52c338a5cf6886aea7b66120bf56f8713f8610d0
jsupinoa12241790-tech <jsupino.a12241790@umak.edu.ph>
2025-12-02 16:42:16 +0800

    Fix WSGI path for nested folders

--- wsgi.py
+++ wsgi.py
@@ -1,19 +1,18 @@
 import os
 import sys
 
-# Make sure project root is on sys.path
+# Get the project root
 project_home = os.path.dirname(__file__)
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# If app resides in nested subfolders (as in this repo), add them too
-subfolder = os.path.join(project_home, "(11- 24) Umak-Lebs Project", "(11- 24) Umak-Lebs Project", "(11- 24) Umak-Lebs Project")
-if os.path.isdir(subfolder) and subfolder not in sys.path:
-    sys.path.insert(0, subfolder)
+# Add nested folder to sys.path manually
+nested_path = os.path.join(project_home, "(UmakLEBS)FILE", "(UmakLEBS)Slips", "(UmakLEBS)MainFile")
+if os.path.isdir(nested_path) and nested_path not in sys.path:
+    sys.path.insert(0, nested_path)
 
-# Import the application object for WSGI servers
+# Import the application object for Gunicorn
 try:
     from app import application
-except Exception:
-    # Fallback: try importing app and exposing app variable
+except ImportError:
     from app import app as application
